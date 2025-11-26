d8f748b2bd67688676dcb7e124ca73c9b7055e8d
Itaysc23 <itay.schwartz@post.runi.ac.il>
2025-11-26 14:07:35 +0200

    Finished homework 4 implementation 3

--- MyString.java
+++ MyString.java
@@ -33,25 +33,23 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        if (str1.length()== 0 || str1.length()<= str2.length()){
-            return true;
-        }else if (str2.length()== 0) {
+        if (str1.length() == 0 || str2.length()>str1.length()){ //if str1 is empty or str2>str1
             return false;
+        } else if (str2.length() == 0){ //if str2 empty
+            return true;
         }
-        for ( int i = 0; i <= str1.length() - str2.length(); i++){
+        for (int i = 0 ; i<=str1.length()-str2.length() ; i++){ //loop over str1 until there isnt space for str2
             Boolean match = true;
-            for( int j = 0; j<str2.length(); j++){
-                if (str1.charAt(i + j) != str2.charAt(j)) {
+            for (int j =0 ; j<str2.length() ; j++){ // loop over str2
+                if (str1.charAt(i+j) != str2.charAt(j)){ //checks for mismatch. if non, will stay true
                     match = false;
-                break; 
+                    break; //exit inner loop 
+                }
+            }
+            if (match){
+                return true;
             }
         }
-
-        if (match) {
-            return true;
-        }
+        return false;
     }
-
-    return false;
-}
 }
